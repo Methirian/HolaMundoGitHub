9692ffee0b7169d135492eb6f4e3e523c2701f67
Methirian <mauro_lara@epam.com>
2021-11-25 16:14:51 -0600

    Second commit

--- src/main/java/HolaMundoGithub.java
+++ src/main/java/HolaMundoGithub.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class HolaMundoGithub {
+public class HolaMundoGithub {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo con github");
+        System.out.println("Usando el segundo commint");
+
+    }
+
 }
